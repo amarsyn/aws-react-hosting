535f8b3a95928265efffc26d4ef548b45ed9fbfe
amarsyn <amarnathb1970@yahoo.com>
2023-06-15 21:15:34 +0530

    changes for v2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Deploy React app on AWS Amplify 15th June 2023
         </p>
         <a
           className="App-link"
