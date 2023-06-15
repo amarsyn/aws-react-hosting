5bb030d698790492abe3e57600116676d9d8ca7c
amarsyn <amarnathb1970@yahoo.com>
2023-06-15 21:26:41 +0530

    changes for v5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey Preetha, what did you have for dinner at Prathamesh's house ?
+          Hey Preetha, what are you preparing for dinner ?
         </p>
         <a
           className="App-link"
